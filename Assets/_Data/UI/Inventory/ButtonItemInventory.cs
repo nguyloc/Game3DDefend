44f1b8e3dcae3800af9908599fb1d25698e0ed8d
Tan Loc <locnntps41055@gmail.com>
2025-02-18 00:04:29 +0700

    Vai tro cua ID

---
+++ Assets/_Data/UI/Inventory/ButtonItemInventory.cs
@@ -0,0 +1,13 @@
+using _Data.UI.Buttons;
+using UnityEngine;
+
+namespace _Data.UI.Inventory
+{
+    public class ButtonItemInventory : ButtonAbstract
+    {
+        protected override void OnClick()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
