eb6ab3b967d72403ae97b48a672ee64c046394aa
Tan Loc <locnntps41055@gmail.com>
2025-02-21 13:24:38 +0700

    Add HP for enemy
    Can Update: Pooling

---
+++ Assets/_Data/UI/Slide/SliderHp.cs
@@ -0,0 +1,19 @@
+﻿using _Data.UI.Slide;
+
+namespace _Data.UI.Slider
+{
+    public abstract class SliderHp : SliderAbstract
+    {
+        protected void FixedUpdate()
+        {
+            this.UpdateSlider();
+        }
+
+        protected virtual void UpdateSlider()
+        {
+            this.slider.value = this.GetValue();
+        }
+
+        protected abstract float GetValue();
+    }
+}
