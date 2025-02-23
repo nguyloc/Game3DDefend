37c30aefc1f5b825b9358ab749638c51b7b6865d
Tan Loc <locnntps41055@gmail.com>
2025-02-23 23:38:46 +0700

    Update Menu Scene

---
+++ Assets/_Data/Sound/Music/Background/BackgroundMenu.cs
@@ -0,0 +1,10 @@
+﻿namespace _Data.Sound.Music.Background
+{
+    public class BackgroundMenu : MusicController
+    {
+        public override string GetName()
+        {
+            return SoundName.BackgroundMusicMenu.ToString();
+        }
+    }
+}
